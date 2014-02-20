e980e066e5e2e8dcd438da5a62fc61b58e97a5f2
flame78 <flame78@gmail.com>
2014-02-20 19:18:51 +0200

    Some start

---
+++ HackWars/HackWarsLib/Hero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackWarsLib
+{
+    public class Hero
+    {
+        public Item Item
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
