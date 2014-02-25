02774df0ac319b6c18ad68f4b8a9e373ca23ae0a
dkasavetova <d.kasavetova@gmail.com>
2014-02-25 20:37:56 +0200

    attack and item

--- HackWars/HackWarsLib/Attack.cs
+++ HackWars/HackWarsLib/Attack.cs
@@ -7,5 +7,20 @@ namespace HackWarsLib
 {
     public class Attack : Item
     {
+        public int attack { get; set; }
+
+        public Attack(int attack)
+        {
+            this.attack = attack;
+        }
+
+        public override void Buy()
+        {
+            attack += attackOfItem;
+        }
+        public override void Sell()
+        {
+            attack -= attackOfItem;
+        }
     }
 }
--- HackWars/HackWarsLib/Item.cs
+++ HackWars/HackWarsLib/Item.cs
@@ -7,5 +7,26 @@ namespace HackWarsLib
 {
     public class Item
     {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int defenseOfItem { get; set; }
+        public int attackOfItem { get; set; }
+
+        public Item(string name, double price, int defenseOfItem, int attackOfItem)
+        {
+            this.name = name;
+            this.price = price;
+            this.defenseOfItem = defenseOfItem;
+            this.attackOfItem = attackOfItem;
+        }
+
+        public virtual void Buy()
+        {
+        }
+
+        public virtual void Sell()
+        {
+        }
+
     }
 }
