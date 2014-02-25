bcfa4e67dab405418f26652c6b4be34b878995de
dkasavetova <d.kasavetova@gmail.com>
2014-02-25 20:33:11 +0200

    add defense

--- HackWars/HackWarsLib/Defense.cs
+++ HackWars/HackWarsLib/Defense.cs
@@ -7,5 +7,20 @@ namespace HackWarsLib
 {
     public class Defense : Item
     {
+        public int defense { get; set; }
+
+        public Defense(int defense)
+        {
+            this.defense = defense;
+        }
+
+        public override void Buy()
+        {
+            defense += defenseOfItem;
+        }
+        public override void Sell()
+        {
+            defense -= defenseOfItem;
+        }
     }
 }
